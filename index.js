cc8278140f4537fc2d7b24bc4b3bc167d95e9b98
Ahallard <K00294870@student.tus.ie>
2025-03-11 11:50:13 +0000

    update-3

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
  * this file is for including javaScript files in our project
  * it is loaded in the html footer so that it doesn't slow down content loading (non-blocking)
  */
+
+jQuery(document).ready(function(){
+    jQuery('#myTable').DataTable();
+    
+    
+    
+    
+// end jQuery
+});
